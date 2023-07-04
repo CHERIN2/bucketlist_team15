b10fcfc4b975f8377b08dc149c314d17d9a52fd3
pinocchio22 <vviian.2@gmail.com>
2023-07-04 20:55:10 +0900

    change

--- bucketlist_team15/lib/main.dart
+++ bucketlist_team15/lib/main.dart
@@ -22,33 +22,25 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+    ),
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text(
+            'Luna828 git branch',
+          ),
+          Text(
+            '',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Luna828 git branch',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+    ), // This trailing comma makes auto-formatting nicer for build methods.
+  );
 }
