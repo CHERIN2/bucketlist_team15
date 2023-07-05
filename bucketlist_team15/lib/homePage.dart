59d2769d1a32da48982c2bd67f83ceb790442307
luna <eskw0701@naver.com>
2023-07-05 17:24:07 +0900

    1. HomePage 변경
    2. shared_preferences 와 provider 패키지 추가

--- bucketlist_team15/lib/homePage.dart
+++ bucketlist_team15/lib/homePage.dart
@@ -1,44 +1,57 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+  const MyHomePage({super.key, required this.name});
+
+  final String name;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text("Hello, World!"),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "${widget.name}님,\n오늘하루도 화이팅 하세요",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700
+                ),
+              ),
+              Expanded(
+                child: ListView(
+                  children: [
+                    ListTile(
+                      leading: IconButton(
+                        icon: Icon(CupertinoIcons.pin),
+                        onPressed: () {},
+                      ),
+                      title: Text(
+                        "text",
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () {},
+        tooltip: '버킷리스트 추가하기',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
--- bucketlist_team15/lib/main.dart
+++ bucketlist_team15/lib/main.dart
@@ -12,12 +12,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      /** debugShowCheckedModeBanner는 에뮬레이터에 오른쪽 맨위에 DEBUG 띠 삭제를 위해 추가하였습니다 */
+      debugShowCheckedModeBanner: false,
       title: 'BucketList_team15',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'bucketList'),
+      home: const MyHomePage(name: 'LUNA'),
     );
   }
 }
