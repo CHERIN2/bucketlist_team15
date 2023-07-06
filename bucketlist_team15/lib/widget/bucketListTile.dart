f6c7442c50901167ef3f2486a223e24c739598b7
luna <eskw0701@naver.com>
2023-07-06 17:23:41 +0900

    1. checkbox check시, bool 값으로 sorting하기

--- bucketlist_team15/lib/widget/bucketListTile.dart
+++ bucketlist_team15/lib/widget/bucketListTile.dart
@@ -1,80 +1,35 @@
 import 'package:bucketlist_team15/BucketList.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../service/bucketList_service.dart';
 
 class bucketListTile extends StatefulWidget {
-  bucketListTile({Key? key, required this.bucketList}) : super(key: key);
-  BucketList? bucketList;
+  bucketListTile({Key? key, required this.index}) : super(key: key);
+
+  final int index;
 
   @override
   State<bucketListTile> createState() => _bucketListTileState();
 }
 
 class _bucketListTileState extends State<bucketListTile> {
-  BucketList? bucketList;
-
-  @override
-  void initState() {
-    super.initState();
-    bucketList = widget.bucketList;
-  }
 
   @override
   Widget build(BuildContext context) {
+    BucketService bucketService = context.read<BucketService>();
+
     return ListTile(
       // checkBox
       leading: Checkbox(
-        value: bucketList!.isChecked,
+        value: bucketService.bucketList[widget.index].isChecked,
         onChanged: (value) {
-          bucketList!.isChecked = value!;
-          setState(() {});
+          bucketService.bucketList[widget.index].isChecked = value!;
+          bucketService.isCheckedBucket(index: widget.index, value: value);
         },
       ),
       // content
-      title: Text(bucketList!.content),
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          // 수정
-          IconButton(
-            icon: Icon(Icons.edit),
-            iconSize: 30,
-            onPressed: (){},
-            color: Colors.black,
-          ),
-          // 삭제
-          IconButton(
-            icon: Icon(Icons.delete),
-            iconSize: 30,
-            onPressed: () {},
-            color: Colors.red,
-          ),
-        ],
-      ),
-    );
-  }
-  State<bucketListTile> createState() => _bucketListTile();
-}
-
-class _bucketListTile extends State<bucketListTile> {
-  BucketList? bucketList;
-
-  @override
-  void initState() {
-    super.initState();
-    bucketList = widget.bucketList;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      // checkBox
-      leading: Checkbox(
-        value: bucketList!.isChecked,
-        fillColor: MaterialStatePropertyAll(Colors.cyan),
-        onChanged: (value) => {bucketList!.isChecked = value!},
-      ),
-      // content
-      title: Text(bucketList!.content),
+      title: Text(bucketService.bucketList[widget.index].content),
       trailing: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
